23fe537e8c35965bfc3920bd8f9f06e43f16b927
patrickmadasoftware <patrickmadasoftware@gmail.com>
2021-04-21 11:53:32 +0200

    sum last digit py

--- fibonacci_sum_last_digit.py
+++ fibonacci_sum_last_digit.py
@@ -2,16 +2,10 @@
 import sys
 
 def calculate_fibonacci(n):
-    if n == 0:
-        return (0, 1)
-    else:
-        a, b = calculate_fibonacci(n // 2)
-        print(a,b)
-        c, d = a * (b * 2 - a), a * a + b * b
-        if n % 2 == 0:
-            return (c, d)
-        else:
-            return (d, c + d)
+    a, b = 0, 1
+    for i in range(n):
+        a, b = b, a + b
+    return a
 
 def fibonacci_sum_naive(p):
     x = calculate_fibonacci(p)
